a3126354b4abf672bc88ae4d9709792e960ff57d
ipqhjjybj <250657661@qq.com>
2023-06-06 15:16:29 +0800

    add code

--- commond_prompt.py
+++ commond_prompt.py
@@ -6,28 +6,6 @@ import random
 from build_dynamic_prompt import *
 
 
-def run_file(in_txt, out_txt, nums=10):
-	button = OneButton()
-	ret = []
-	f = open(f"./ori_inputs/{in_txt}", "r")
-	for line in f:
-		type_of_image, subject = line.strip().split(',')
-		special_words = ""
-		for i in range(nums):
-			prompt = button.get_prompt(type_of_image, subject, special_words)
-			ret.append(prompt)
-	f.close()
-
-	if not os.path.exists(ori_outputs_dir):
-		os.mkdir(ori_outputs_dir)
-
-	f = open(f"{ori_outputs_dir}/{out_txt}", "w")
-	for prompt in ret:
-		f.write(prompt + "\n")
-	f.close()
-	return ret
-
-
 def test(special_words, type_of_image, subject):
 	button = OneButton()
 	#print(type_of_image, subject, special_words)
